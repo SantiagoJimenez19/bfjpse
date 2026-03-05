a7467b46da590d855bc733e270b1f7e41be2c557
SantiagoJimenez19 <ji378662@uaeh.edu.mx>
2026-03-05 23:17:00 +0000

    asd

--- btjpse/__init__.py
+++ btjpse/__init__.py
@@ -1,6 +1,9 @@
 from itertools import product
 import networkx as nx
 
+def suma(a,b):
+  return a+b
+
 def is_hamltonian_cycle(graph, cycle):
   """Checks if cycle is a hamiltonian cycle in graph
   graph is a Networkx graph, and cycle is a list of verticex"""
